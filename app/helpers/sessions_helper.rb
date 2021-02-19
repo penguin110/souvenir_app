694f6439b19d01ce06a9da06f714cf76879ee851
penguin110 <saboten.saboten.1010@gmai.com>
2021-02-20 00:13:10 +0900

    修正しました

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,11 @@ module SessionsHelper
     def current_user?(user)
         user == current_user
     end
+    def current_user
+        if session[:user_id]
+            current_user = session[:user_id]
+        else
+            current_user = ""
+        end
+    end
 end
