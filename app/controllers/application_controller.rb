5b39d5d3364008bf4b7b32e527af455296342ee5
penguin110 <saboten.saboten.1010@gmai.com>
2021-03-14 11:30:36 +0900

    県名が重複してしまう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
-  before_action :sidebar_val
+  before_action :sidebar_val, :top_val
+
+  def top_val
+    @prefectures = Prefecture.all
+    @region_num = Prefecture.pluck(:region_id).uniq.count
+    @prefecture = Prefecture.find_by(prefecture_id: params[:prefecture_id])
+
+    @regions = Region.all
+  end
 
   
   def login_required
